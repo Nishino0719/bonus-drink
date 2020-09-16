66239e6dc80eb1893c91fb4f4b7e4e3ea3ced6c5
Nishino0719 <nishino0719@icloud.com>
2020-09-16 17:37:31 +0900

    create answer of bonus drink

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    if amount == 0 
+      0
+    elsif amount % 2 == 1
+      amount * 3 / 2 
+    elsif amount % 2 == 0
+      (amount - 1) * 3 / 2 + 1
+    end
   end
 end
